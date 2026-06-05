f72003ca81aa35e3ca4b29e51a885c7aa041741e
YellowGun11 <yellowgun11@163.com>
2026-06-05 09:39:24 +0800

    屏蔽素材新增库中的发布按钮，屏蔽评论组件中的@按钮，屏蔽首选项中的主题切换

--- src/app/main/ui/comments.cljs
+++ src/app/main/ui/comments.cljs
@@ -665,7 +665,6 @@
              (on-submit))))]
 
     [:div {:class (stl/css :form-buttons-wrapper)}
-     [:> mentions-button*]
      (when (some? on-cancel)
        [:> button* {:variant "ghost"
                     :type "button"
